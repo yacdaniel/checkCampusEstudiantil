0b21509ed05c3ab9ecec663266027538017d6c09
Yacdaniel Hurtado <yacdaniel@gmail.com>
2017-02-22 00:25:40 -0600

    use firefox

--- src/main/java/com/yacdaniel/selenium/SeleniumBase.java
+++ src/main/java/com/yacdaniel/selenium/SeleniumBase.java
@@ -1,7 +1,7 @@
 package com.yacdaniel.selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,7 +14,7 @@ public class SeleniumBase {
 
     @BeforeMethod(alwaysRun = true)
     public void beforeTest(){
-        navegador = new ChromeDriver();
+        navegador = new FirefoxDriver();
         navegador.manage().window().maximize();
     }
 
